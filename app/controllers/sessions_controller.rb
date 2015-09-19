7f3ec4355918696c2b4176a52bb62408a4aab129
alakshm6 <alakshm6@ncsu.edu>
2015-09-19 12:36:49 -0400

    Session Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,18 @@
 class SessionsController < ApplicationController
-  # "Create" a login, aka "log the user in"
+  def new
+  end
+
   def create
-    if user = User.authenticate(params[:username], params[:password])
-          # Save the user ID in the session so it can be used in
-          # subsequent requests
-          session[:current_user_id] = user.id
-          redirect_to root_url
+    user = Admin.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to root_url
+    else
+      flash[:danger] = 'email/password is invalid'
+      render 'new'
     end
   end
 
-  # "Delete" a login, aka "log the user out"
   def destroy
-    # Remove the user id from the session
-    @_current_user = session[:current_user_id] = nil
-    redirect_to root_url
   end
+
 end
