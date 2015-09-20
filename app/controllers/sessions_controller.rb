4b7a3d94a7bcd6cd30c1340c872b4b8543aa3333
alakshm6 <alakshm6@ncsu.edu>
2015-09-20 11:55:37 -0400

    Adding session details and segregation based on user type

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,25 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      redirect_to root_url
+      session[:current_user_id] = user.id
+      user_type = User.find_by_id(session[:current_user_id]).user_type
+      if user_type == "A"
+        respond_to do |format|
+          format.html{ redirect_to new_book_path, notice: "Logged in successfully"}
+        end
+      elsif user_type == 'P'
+        respond_to do |format|
+          format.html{ redirect_to new_user_path, notice: "Pre configured logged in"}
+        end
+      else user_type == 'U'
+        respond_to do |format|
+          format.html{redirect_to new_checkout_history_path, notice: "User logged in"}
+        end
+      end
     else
-      flash[:danger] = 'email/password is invalid'
-      render 'new'
+      respond_to do |format|
+        format.html{ redirect_to login_path, notice: "Pre configured logged in"}
+      end
     end
   end
 
